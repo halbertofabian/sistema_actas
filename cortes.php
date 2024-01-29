639c10480f33a0c42ccb7369c56073c02e28e821
Your Name <victor081196>
2024-01-29 14:23:32 -0600

    Cambios actas

--- cortes.php
+++ cortes.php
@@ -31,7 +31,7 @@ if (isset($_GET['cliente'])) {
     curl_close($curl);
 
     if ($err) {
-        echo "cURL Error #:" . $err;
+        echo "CURL Error #:" . $err;
     } else {
         $mensajes = json_decode($response, true);
         if ($mensajes === null) {
@@ -41,18 +41,26 @@ if (isset($_GET['cliente'])) {
             $reversedArray = array_reverse($mensajes);
             // var_dump($reversedArray);
             // return;
-            // $servicios = Modelo::mdlMostrarServicios();
+            $servicios = Modelo::mdlMostrarServicios();
             $totalActas = 0;
             $totalRfc = 0;
+            $totalCfe = 0;
+            $totalNss = 0;
             foreach ($reversedArray as $key => $msg) {
-                $opciones = array('ACTAS', 'RFC', 'CFE', 'NSS');
+                // $opciones = array('ACTAS', 'RFC', 'CFE', 'NSS');
                 if ($autor == $msg['author']) {
-                    $informacionPedido = extraerInformacionPedido($msg['body'], $opciones);
+                    $informacionPedido = extraerInformacionPedido($msg['body'], $servicios);
                     if ($informacionPedido !== null && isset($informacionPedido['ACTAS']) && $informacionPedido['ACTAS'] != "") {
                         $totalActas += intval($informacionPedido['ACTAS']);
                     }
                     if ($informacionPedido !== null && isset($informacionPedido['RFC']) && $informacionPedido['RFC'] != "") {
                         $totalRfc += intval($informacionPedido['RFC']);
+                    }
+                    if ($informacionPedido !== null && isset($informacionPedido['CFE']) && $informacionPedido['CFE'] != "") {
+                        $totalCfe += intval($informacionPedido['CFE']);
+                    }
+                    if ($informacionPedido !== null && isset($informacionPedido['NSS']) && $informacionPedido['NSS'] != "") {
+                        $totalNss += intval($informacionPedido['NSS']);
                     }
                 } else {
                     continue;
@@ -63,6 +71,8 @@ if (isset($_GET['cliente'])) {
 
             $precio_total_actas = 0;
             $precio_total_rfc = 0;
+            $precio_total_cfe = 0;
+            $precio_total_nss = 0;
             foreach ($paquete as $key => $pqt) {
                 if ($pqt['srv_nombre'] == "ACTAS") {
                     $precio_total_actas = $totalActas * $pqt['prc_precio'];
@@ -70,13 +80,23 @@ if (isset($_GET['cliente'])) {
                 if ($pqt['srv_nombre'] == "RFC") {
                     $precio_total_rfc = $totalRfc * $pqt['prc_precio'];
                 }
+                if ($pqt['srv_nombre'] == "CFE") {
+                    $precio_total_cfe = $totalCfe * $pqt['prc_precio'];
+                }
+                if ($pqt['srv_nombre'] == "NSS") {
+                    $precio_total_nss = $totalNss * $pqt['prc_precio'];
+                }
             }
 
             echo 'Total de actas: ' . $totalActas . '<br>';
             echo 'Total de rfc: ' . $totalRfc . '<br>';
+            echo 'Total de cfe: ' . $totalCfe . '<br>';
+            echo 'Total de nss: ' . $totalNss . '<br>';
             var_dump($paquete);
             echo 'Precio total actas: ' . $precio_total_actas . '<br>';
             echo 'Precio total rfc: ' . $precio_total_rfc . '<br>';
+            echo 'Precio total cfe: ' . $precio_total_cfe . '<br>';
+            echo 'Precio total nss: ' . $precio_total_nss . '<br>';
         }
     }
 }
@@ -89,14 +109,14 @@ function extraerInformacionPedido($texto, $opciones)
 
         foreach ($opciones as $opcion) {
             // Para cada opción, buscar la correspondiente línea en el texto
-            preg_match("/{$opcion}: (.*)/i", $texto, $coincidencia);
+            preg_match("/{$opcion['srv_nombre']}:(.*)/i", $texto, $coincidencia);
 
             if (!empty($coincidencia)) {
                 $valor = $coincidencia[1];
-                $informacion[$opcion] = $valor;
+                $informacion[$opcion['srv_nombre']] = $valor;
             } else {
                 // Si no se encuentra la línea, asignar un valor por defecto o null según sea necesario
-                $informacion[$opcion] = null;
+                $informacion[$opcion['srv_nombre']] = null;
             }
         }
 
